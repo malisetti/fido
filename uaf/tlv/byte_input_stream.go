31787d3bfaaaf9d0bbc47650c4f3f8a57fd82c47
Seshachalam Malisetti <seshachalam.malisetti@imaginea.com>
2016-10-25 14:47:13 +0530

    Think

--- uaf/tlv/byte_input_stream.go
+++ uaf/tlv/byte_input_stream.go
@@ -1,14 +1,10 @@
 package tlv
 
-import (
-	"bytes"
-)
+import ()
 
 type ByteInputStream struct {
 	Data []byte
 }
 
 func (bsi *ByteInputStream) Read(numberOfBytes int) []byte {
-	readBytes := bytes.Buffer{}
-	readBytes.Write(bsi.Data)
 }
